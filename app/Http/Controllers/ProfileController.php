d2798fd8285a9222825821057577cc39f5fdae8c
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-25 09:50:43 +0600

    final Assignment

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,20 +3,18 @@
 namespace App\Http\Controllers;
 
 class ProfileController extends Controller {
-    // The index method receives the $id from the route
+
     public function index($id) {
-        // Declare variables with values
+
         $name = "Donal Trump";
         $age = "75";
 
-        // Create the associative array with id, name, and age
         $data = [
             'id' => $id,
             'name' => $name,
             'age' => $age,
         ];
 
-        // Set cookie as per the question's specifications
         $cookie_name = 'access_token';
         $cookie_value = '123-XYZ';
         $minutes = 1;
@@ -25,7 +23,6 @@ class ProfileController extends Controller {
         $secure = false;
         $httpOnly = true;
 
-        // Return response with data, status code 200, and the cookie
         return response()->json($data, 200)
             ->cookie($cookie_name, $cookie_value, $minutes, $path, $domain, $secure, $httpOnly);
     }
