867ca2411eb4fea1ff3bebbe3568670be3b7500c
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-11-27 01:01:40 +0600

    update

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DemoController extends Controller {
+    function DemoAction() {
+        return "My name is Mejbaul Mubin";
+    }
+
+    function RouteTwo() {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/demo', function () {
+return "My name is Mejbaul Mubin";
+});
+ */
+Route::get('/demo', [DemoController::class, 'DemoAction']);
+
+Route::get('/hello/{name}/{age}', [DemoController::class, 'RouteTwo']);
 
 Route::get('/', function () {
     return view('welcome');
